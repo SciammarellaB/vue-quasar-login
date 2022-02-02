2631b29ca75c18f815ae8ec4da0c16dc1ca44de1
BrennoSciammarella <brenno.s97@gmail.com>
2022-02-01 21:29:09 -0300

    Base

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
+
+createApp(App).use(Quasar, quasarUserOptions).use(router).mount('#app')
---
+++ src/quasar-user-options.js
@@ -0,0 +1,12 @@
+
+import './styles/quasar.scss'
+import iconSet from 'quasar/icon-set/mdi-v4.js'
+import '@quasar/extras/mdi-v4/mdi-v4.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+}
