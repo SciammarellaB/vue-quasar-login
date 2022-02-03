fdbee09621faf64b5d549cd3678985cd32bb9248
BrennoSciammarella <brenno.s97@gmail.com>
2022-02-03 06:24:24 -0300

    Deploy

---
+++ precache-manifest.ec03a55472f2d10788e5026f269b4657.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3f24d450211fcea833",
+    "url": "/vue-quasar-login/css/app.1fd6af07.css"
+  },
+  {
+    "revision": "046c6b5b8238d6881f85",
+    "url": "/vue-quasar-login/css/chunk-vendors.6d88185e.css"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/vue-quasar-login/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/vue-quasar-login/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "62b39ab431e50123b0e7aeb76baf4aa8",
+    "url": "/vue-quasar-login/img/compLogo.62b39ab4.png"
+  },
+  {
+    "revision": "ad4fab8c0fa709a32c8adbc9b1d723c6",
+    "url": "/vue-quasar-login/index.html"
+  },
+  {
+    "revision": "0c3f24d450211fcea833",
+    "url": "/vue-quasar-login/js/app.a858c340.js"
+  },
+  {
+    "revision": "62d58584e9fe13003334",
+    "url": "/vue-quasar-login/js/chunk-2d2086b7.bdaa170f.js"
+  },
+  {
+    "revision": "663ff8532c0a9676c089",
+    "url": "/vue-quasar-login/js/chunk-4fa2a018.61a5ce9d.js"
+  },
+  {
+    "revision": "c121bf0a0a7f245c8eb3",
+    "url": "/vue-quasar-login/js/chunk-810a2a6e.535471d9.js"
+  },
+  {
+    "revision": "046c6b5b8238d6881f85",
+    "url": "/vue-quasar-login/js/chunk-vendors.73bca731.js"
+  },
+  {
+    "revision": "58a720000535bc9ecbdcfbdf340d66b9",
+    "url": "/vue-quasar-login/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-quasar-login/robots.txt"
+  }
+]);
